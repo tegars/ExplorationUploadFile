a4f3971a174eaa2774814299b85abc7544158d08
tegarsweefer <tegar.s@weefer.co.id>
2020-05-19 16:33:00 +0700

    validate extentions

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -16,7 +16,13 @@ namespace ExplorationUploadFile.Controllers
         [RequestSizeLimit(8000000)]
         public async Task<ActionResult> Single(IFormFile file)
         {
+            string[] permittedExtensions = { ".csv" };
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                return BadRequest("Only Allowed CSV File");
+
             var filePath = Path.Combine(@"c:\Project\testing2", file.FileName);
+            //var filePath = Path.Combine("Upload", file.FileName);
             using (var stream = System.IO.File.Create(filePath))
             {
                 await file.CopyToAsync(stream);
