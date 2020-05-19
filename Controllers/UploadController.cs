7d99afd365e99b9e9e9c0c7f83ce6e7347192f26
tegarsweefer <tegar.s@weefer.co.id>
2020-05-19 16:27:44 +0700

    success upload file

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ExplorationUploadFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("Single")]
+        [RequestSizeLimit(8000000)]
+        public async Task<ActionResult> Single(IFormFile file)
+        {
+            var filePath = Path.Combine(@"c:\Project\testing2", file.FileName);
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+    }
+}
