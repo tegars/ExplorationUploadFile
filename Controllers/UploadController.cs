55066ce45ace3536c94d084fb5b4e83f8151adc7
tegarsweefer <tegar.s@weefer.co.id>
2020-05-28 16:18:34 +0700

    upload with rename was done

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using ExplorationUploadFile.Helpers;
 
 namespace ExplorationUploadFile.Controllers
 {
@@ -21,12 +22,34 @@ namespace ExplorationUploadFile.Controllers
             if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
                 return BadRequest("Only Allowed CSV File");
 
-            var filePath = Path.Combine(@"c:\Project\testing2", file.FileName);
-            //var filePath = Path.Combine("Upload", file.FileName);
+            //var filePath = Path.Combine(@"c:\Project\testing2", file.FileName);
+            var filePath = Path.Combine("Upload", file.FileName);
             using (var stream = System.IO.File.Create(filePath))
-            {
                 await file.CopyToAsync(stream);
+
+            return Ok();
+        }
+        [HttpPost("WithRename")]
+        [RequestSizeLimit(8000000)]
+        public async Task<ActionResult> WithRename(IFormFile file)
+        {
+            string[] permittedExtensions = { ".xls" };
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                return BadRequest("Only Allowed XLS File");
+
+            string fileName = file.FileName;
+            bool check = System.IO.File.Exists("Upload/"+fileName);
+            while (check)
+            {
+                fileName = fileName.Replace(".xls","")+"_"+Helper.RandomString(5)+".xls";
+                check = System.IO.File.Exists("Upload/" + fileName);
             }
+
+            var filePath = Path.Combine("Upload", fileName);
+            using (var stream = System.IO.File.Create(filePath))
+                await file.CopyToAsync(stream);
+
             return Ok();
         }
     }
